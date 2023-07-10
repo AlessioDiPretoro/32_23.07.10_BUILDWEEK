1a6d202d94f5117cdb9a0773a1724efe7188b68f
Alessandro Giorgino <alessandrogiorgino95@gmail.com>
2023-07-10 13:12:46 +0200

    apgina ricerca con album

--- script/serch.js
+++ script/serch.js
@@ -5,12 +5,40 @@ form.addEventListener("submit", function (e) {
   let input = document.querySelector("input").value;
   console.log(input);
 
+  const contenitoreRicerca = document.getElementById("contenitoreRicerca");
+
   const serch = async function () {
     try {
       let response = await fetch(url + input);
       console.log(response);
       let data = await response.json();
       console.log(data);
+      // console.log(data.data[0]);
+
+      //creo array vuoto da popolare con album
+      let arrayAlbum = [];
+      //ciclo array data, pesco gli album e lizeppo dentro l'array
+
+      data.data.forEach((dato) => {
+        let titolo = dato.album.title;
+        arrayAlbum.push(titolo);
+        //creiamo i contenitori per gli album
+        const artistaConFoto = document.createElement("div");
+        artistaConFoto.innerHTML = `
+        <div>
+        <img src="${dato.album.cover_small}" \>
+        </div>
+        `;
+
+        contenitoreRicerca.appendChild(artistaConFoto);
+      });
+      console.log(arrayAlbum);
+
+      //compilare in abe alla ricerca delle card:
+      //  let artistaTrovato = document.createElement('div')
+      // il primo risultato è un div con background image l'artista e le scritte bla bla
+      //e che fa da link alla pagina dell'artista con le canzoni
+      //  artistaTrovato.style= "backgroundImage: url('')"
     } catch (a) {
       console.log(a);
     }
