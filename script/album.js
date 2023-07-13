4e69fdfe99e38c744f94bd6724f057123f2307bf
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-07-13 19:02:09 +0200

    ok

--- script/album.js
+++ script/album.js
@@ -30,6 +30,7 @@ const artist = async function () {
     if (second < 10) {
       second = `0${second}`;
     }
+
     artistName.innerHTML = `
     <div id="immagineAlbum"">
     <img
@@ -52,6 +53,41 @@ const artist = async function () {
     </div>
   </div>
 `;
+    let divCard = document.getElementById("CardContinerAlbum");
+    artist.tracks.data.forEach((e, n) => {
+      let div = document.createElement("div");
+      let duration = e.duration / 60;
+      let minute = Math.floor(duration);
+      let second = e.duration - minute * 60;
+      div.classList.add(
+        "d-flex",
+        "align-items-center",
+        "justify-content-evenly",
+        "my-3",
+        "my-md-4"
+      );
+      div.innerHTML = `  <div class="col col-1 d-none d-md-block">${n + 1}</div>
+
+       <div
+         class="col col-8 d-flex flex-column justify-content-between align-items-md-center flex-md-row ps-2"
+      >
+         <div class="">
+          <h2 class="m-0">${e.title}</h2>
+        </div>
+         <div class="">
+           <p class="m-0">${e.rank}</p>
+        </div>
+       </div>
+       <div class="col col-2">
+      <p class="d-none d-md-flex m-0 text-end justify-content-end align-items-center">${minute}:${second} <i class="fa-regular fa-clock ms-2"></i>
+         </p>
+        <p class="d-md-none">
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </p>
+     </div>
+  `;
+      divCard.appendChild(div);
+    });
 
     // console.log("artist", artist);
     const draw = function (img) {
