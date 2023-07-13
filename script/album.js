03cfd562037a4b5d996c481a8b1f52cc89155853
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-07-13 19:22:55 +0200

    ale

--- script/album.js
+++ script/album.js
@@ -53,92 +53,41 @@ const artist = async function () {
 `;
 
     const scatolaTracce = document.getElementById("songsData");
+    let divCard = document.getElementById("cardContinerAlbum");
+    artist.tracks.data.forEach((e, n) => {
+      let div = document.createElement("div");
+      let duration = e.duration / 60;
+      let minute = Math.floor(duration);
+      let second = e.duration - minute * 60;
+      div.classList.add(
+        "d-flex",
+        "align-items-center",
+        "justify-content-evenly",
+        "my-3",
+        "my-md-4"
+      );
+      div.innerHTML = `  <div class="col col-1 d-none d-md-block">${n + 1}</div>
 
-    // console.log("artist", artist);
-    const draw = function (img) {
-      let canvas = document.createElement("canvas");
-      let c = canvas.getContext("2d");
-      c.width = canvas.width = img.clientWidth;
-      c.height = canvas.height = img.clientHeight;
-      c.clearRect(0, 0, c.width, c.height);
-      c.drawImage(img, 0, 0, img.clientWidth, img.clientHeight);
-      return c;
-    };
-
-    // scompone pixel per pixel e ritorna un oggetto con una mappa della loro frequenza nell'immagine
-    const getColors = function (c) {
-      let col,
-        colors = {};
-      let pixels, r, g, b, a;
-      r = g = b = a = 0;
-      pixels = c.getImageData(0, 0, c.width, c.height);
-      for (let i = 0, data = pixels.data; i < data.length; i += 4) {
-        r = data[i];
-        g = data[i + 1];
-        b = data[i + 2];
-        a = data[i + 3];
-        if (a < 255 / 2) continue;
-        col = rgbToHex(r, g, b);
-        if (!colors[col]) colors[col] = 0;
-        colors[col]++;
-      }
-      return colors;
-    };
-
-    // trova il colore più ricorrente data una mappa di frequenza dei colori
-    const findMostRecurrentColor = function (colorMap) {
-      let highestValue = 0;
-      let mostRecurrent = null;
-      for (const hexColor in colorMap) {
-        if (colorMap[hexColor] > highestValue) {
-          mostRecurrent = hexColor;
-          highestValue = colorMap[hexColor];
-        }
-      }
-      return mostRecurrent;
-    };
-
-    // converte un valore in rgb a un valore esadecimale
-    const rgbToHex = function (r, g, b) {
-      if (r > 255 || g > 255 || b > 255) {
-        throw "Invalid color component";
-      } else {
-        return ((r << 16) | (g << 8) | b).toString(16);
-      }
-    };
-
-    // inserisce degli '0' se necessario davanti al colore in esadecimale per renderlo di 6 caratteri
-    const pad = function (hex) {
-      return ("000000" + hex).slice(-6);
-    };
-
-    //da chiedere a stefano
-    const start = function () {
-      // prendo il riferimento all'immagine del dom
-      let imgReference = document.querySelectorAll(".cardsAlbum");
-      console.log("imgReference", imgReference);
-
-      imgReference.forEach((immagine) => {
-        console.log("immagine", immagine);
-        immagine.crossOrigin = "Anonymous";
-        // creo il context 2d dell'immagine selezionata
-        let context = draw(immagine);
-        console.log(context);
-        // creo la mappa dei colori più ricorrenti nell'immagine
-        let allColors = getColors(context);
-
-        // trovo colore più ricorrente in esadecimale
-        let mostRecurrent = findMostRecurrentColor(allColors);
-
-        // se necessario, aggiunge degli '0' per rendere il risultato un valido colore esadecimale
-        let mostRecurrentHex = pad(mostRecurrent);
-
-        // console.log del risultato
-        console.log(mostRecurrentHex);
-        immagine.parentElement.parentElement.style.backgroundColor = "#" + mostRecurrentHex + "99";
-      });
-    };
-    start();
+       <div
+         class="col col-8 d-flex flex-column justify-content-between align-items-md-center flex-md-row ps-2"
+      >
+         <div class="">
+          <h2 class="m-0">${e.title}</h2>
+        </div>
+         <div class="">
+           <p class="m-0">${e.rank}</p>
+        </div>
+       </div>
+       <div class="col col-2">
+      <p class="d-none d-md-flex m-0 text-end justify-content-end align-items-center">${minute}:${second} <i class="fa-regular fa-clock ms-2"></i>
+         </p>
+        <p class="d-md-none">
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </p>
+     </div>
+  `;
+      divCard.appendChild(div);
+    });
 
     // crea un canvas con l'immagine e ne ritorno il context 2d
   } catch (err) {
